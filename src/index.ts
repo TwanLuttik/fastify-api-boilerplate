0c85db9191dcfa3e75dd027c9e14e4fb7c9e5ab0
TwanLuttik <twanluttik@gmail.com>
2024-04-01 21:32:37 -0700

    updated deps and removed old logger to default logger from fastify

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { registerPlugins } from './utils/FastPlugins';
 
 import colors from 'colors';
 
-export const fast: FastifyInstance = fastify({ trustProxy: true });
+export const fast: FastifyInstance = fastify({ trustProxy: true, logger: true });
 
 export const initializeServer = async (incomingConfig?: InitilizeConfig) => {
 	// register plugins
@@ -17,18 +17,6 @@ export const initializeServer = async (incomingConfig?: InitilizeConfig) => {
 
 	// Register the routes
 	await registerRoutes();
-
-	// Customized logger
-	fast.addHook('onSend', (req: FastifyRequest<any>, res: FastifyReply<any>, payload, next) => {
-		next();
-		if (req.method === 'OPTIONS') return;
-
-		const message = `${colors.dim(`[${new Date().toTimeString().split(' ')[0]}]`)} ${colors.green(`[${req.method}]`)} ${colors.yellow(
-			`[${res.statusCode}]`
-		)} ${colors.blue('[' + res.getResponseTime().toFixed(2) + 'ms]')} => ${colors.red(req.url)} ${colors.white(`> ${req.ip}`)}`;
-
-		console.log(message);
-	});
 
 	fast.listen({ port: incomingConfig?.port || 8080, host: incomingConfig?.host || '0.0.0.0' }, (err, address) => {
 		if (err) {
